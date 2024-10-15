ee0abc6ede21ee744e8361fea2a49e77d99bacf8
redaccionHorizon <redaccioniahorizon@gmail.com>
2024-10-15 18:02:33 -0300

    Actualización del build y ajustes en la configuración

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
@@ -10,14 +10,16 @@ function App() {
     <div className="App">
       <div className="max-w-[90%] md:max-w-[50%] mx-auto">
         <Routes>
-          <Route element={<Login/>} path="/login"/>
-          <Route element={<Register/>} path="/register"/>
-          <Route element={<HomePage/>} path="/"/>
-          <Route element={<Profile/>} path="/profile" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Register />} path="/register" />
+          <Route element={<HomePage />} path="/home" />
+          <Route element={<Profile />} path="/profile" />
+          {/* Redirigir la raíz a /login */}
+          <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </div>
     </div>
   );
-};
+}
 
 export default App;
